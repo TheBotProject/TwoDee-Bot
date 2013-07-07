3766c13a30ddc1c1863ad76c8286806958eefe8b
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-07 18:21:21 +0200

    Link to the big version of the image

--- plugins/pixiv.js
+++ plugins/pixiv.js
@@ -9,7 +9,7 @@ module.exports = function (client, channelName) {
 
 			csv().from.string(data).to.array(function (arr) {
 				arr = arr[0];
-				client.say(channelName, arr[3] + ' [' + arr[5] + '] - http://pixiv.net/member_illust.php?mode=medium&illust_id=' + id);
+				client.say(channelName, arr[3] + ' [' + arr[5] + '] - http://pixiv.net/member_illust.php?mode=big&illust_id=' + id);
 			});
 		});
 	}
