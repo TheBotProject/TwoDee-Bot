b5ad06919ead33d0105457e3f9adebbeed633008
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-07 22:24:11 +0200

    Add mal plugin modes

--- plugins/mal.js
+++ plugins/mal.js
@@ -5,8 +5,8 @@ var Q = require('q');
 
 module.exports = function (client, channelName) {
 
-	function searchAnime(query) {
-		google.resultsPerPage = 3;
+	function searchAnime(query, limit) {
+		google.resultsPerPage = limit;
 
 		google('site:myanimelist.net/anime/ ' + query, function (err, next, links) {
 			if (err) return;
@@ -15,38 +15,57 @@ module.exports = function (client, channelName) {
 				client.say(channelName, 'Sorry, no results for: ' + query);
 			}
 
-			Q.all(
-				links.map(function (link) {
-					var deferred = Q.defer();
-					var match = link.link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
-					if (match) {
-						request('http://mal-api.com/anime/' + match[1], function (err, r, data) {
-							if (err) {
-								deferred.reject(err);
-							} else {
-								deferred.resolve(JSON.parse(data));
-							}
-						});
-					} else {
-						deferred.resolve(undefined);
-					}
-					return deferred.promise;
-				})
-			).then(function (datas) {
-				for (var i = 0; i < datas.length; ++i) {
-					var data = datas[i];
-					if (!data) continue;
+			if (limit === 1) {
+				var link = links[0];
+				var match = link.link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
+				if (match) {
+					request('http://mal-api.com/anime/' + match[1], function (err, r, data) {
+						if (err) return;
 
-					client.say(channelName, ent.decode(data.title) + ' (' + (data.episodes ? data.episodes : '?') + ' episodes) - http://myanimelist.net/anime/' + data.id);
+						data = JSON.parse(data);
+						client.say(channelName, ent.decode(data.title) + ' (' + (data.episodes ? data.episodes : '?') + ' episodes) - http://myanimelist.net/anime/' + data.id);
+					});
+				} else {
+					next();
 				}
-			}).done();
+			} else {
+				Q.all(
+					links.map(function (link) {
+						var deferred = Q.defer();
+						var match = link.link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
+						if (match) {
+							request('http://mal-api.com/anime/' + match[1], function (err, r, data) {
+								if (err) {
+									deferred.reject(err);
+								} else {
+									deferred.resolve(JSON.parse(data));
+								}
+							});
+						} else {
+							deferred.resolve(undefined);
+						}
+						return deferred.promise;
+					})
+				).then(function (datas) {
+					for (var i = 0; i < datas.length; ++i) {
+						var data = datas[i];
+						if (!data) continue;
+
+						client.say(channelName, ent.decode(data.title) + ' (' + (data.episodes ? data.episodes : '?') + ' episodes) - http://myanimelist.net/anime/' + data.id);
+					}
+
+				}).done();
+			}
 		});
 	}
 
 	return {
 		commands: {
 			mal: function (from, message) {
-				searchAnime(message);
+				searchAnime(message, 1);
+			},
+			mal3: function (from, message) {
+				searchAnime(message, 3);
 			}
 		}
 	};
