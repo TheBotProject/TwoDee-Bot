b7382d68295160633eb82f9d659613be546e279b
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-07 20:54:50 +0200

    Order !mal results by google result order

--- plugins/mal.js
+++ plugins/mal.js
@@ -1,6 +1,7 @@
 ﻿var google = require('google');
 var request = require('request');
 var ent = require('ent');
+var Q = require('q');
 
 module.exports = function (client, channelName) {
 
@@ -14,17 +15,27 @@ module.exports = function (client, channelName) {
 				client.say(channelName, 'Sorry, no results for: ' + query);
 			}
 
-			for (var i = 0; i < links.length; ++i) {
-				var match = links[i].link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
-				if (match) {
-					request('http://mal-api.com/anime/' + match[1], function (err, r, data) {
-						if (err) return;
-
-						data = JSON.parse(data);
-						client.say(channelName, ent.decode(data.title) + ' (' + (data.episodes ? data.episodes : '?') + ' episodes) - http://myanimelist.net/anime/' + data.id);
-					});
+			Q.all(
+				links.map(function (link) {
+					var deferred = Q.defer();
+					var match = link.link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
+					if (match) {
+						request('http://mal-api.com/anime/' + match[1], function (err, r, data) {
+							if (err) {
+								deferred.reject(err);
+							} else {
+								deferred.resolve(JSON.parse(data));
+							}
+						});
+					}
+					return deferred.promise;
+				})
+			).then(function (datas) {
+				for (var i = 0; i < datas.length; ++i) {
+					var data = datas[i];
+					client.say(channelName, ent.decode(data.title) + ' (' + (data.episodes ? data.episodes : '?') + ' episodes) - http://myanimelist.net/anime/' + data.id);
 				}
-			}
+			}).done();
 		});
 	}
 
