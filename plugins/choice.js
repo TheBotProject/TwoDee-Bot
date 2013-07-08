24bb30219d6e4529af3570c19d72f467c27794da
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-08 22:43:08 +0200

    Add choice command

---
+++ plugins/choice.js
@@ -0,0 +1,20 @@
+﻿var readLine = require("csv");
+
+module.exports = function (client, channelName) {
+
+	function random(min, max) {
+		return min + Math.floor(Math.random() * ((max - min) + 1));
+	}
+
+	return {
+		commands: {
+			choice: function (from, message) {
+				csv().from.string(message, { delimiter: ' ' }).to.array(function (data) {
+					data = data[0];
+
+					client.say(channelName, from + ': ' + data[random(0, data.length - 1)]);
+				});
+			}
+		}
+	};
+}
