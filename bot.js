a08e448f68d418dbcbf9854715edd04dd095ba63
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-08 21:41:14 +0200

    Put reddit into its own plugin

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 ﻿var irc = require('irc');
-var redwrap = require('redwrap');
 var fs = require('fs');
-var ent = require('ent');
 
 /*
 var channelName = '#TwoDeeTest';
@@ -9,13 +7,10 @@ var reddits = 'all';
 */
 var botName = 'MoeBot';
 var channelName = '#TwoDee';
-var reddits = 'awwnime+pantsu+melanime+luckyyuri+kyoaniyuri+patchuu+moescape+imouto+ZettaiRyouiki';
 var client;
 
 var messageHandlers = [];
 var commands = {};
-
-var lastSeen = [];
 
 function registerPlugin(instance) {
 	if (instance instanceof Function) {
@@ -55,8 +50,16 @@ function initIRC(pw) {
 
 	client.on('join', function (channel, user) {
 		if (channel === channelName && user === client.nick) {
-			console.log('Connected to IRC - starting requests');
-			setInterval(searchNew, 30 * 1000);
+			console.log('Connected to IRC');
+
+			registerPlugin('./plugins/reddit');
+			registerPlugin('./plugins/youtube');
+			registerPlugin('./plugins/imgur');
+			registerPlugin('./plugins/mal');
+			registerPlugin('./plugins/pixiv');
+			registerPlugin('./plugins/sauce');
+			registerPlugin('./plugins/whattowatch');
+			registerPlugin('./plugins/pet');
 		}
 	});
 
@@ -71,53 +74,6 @@ function initIRC(pw) {
 				messageHandlers[i](from, message);
 			}
 		}
-	});
-
-	registerPlugin('./plugins/youtube');
-	registerPlugin('./plugins/imgur');
-	registerPlugin('./plugins/mal');
-	registerPlugin('./plugins/pixiv');
-	registerPlugin('./plugins/sauce');
-	registerPlugin('./plugins/whattowatch');
-	registerPlugin('./plugins/pet');
-}
-
-function searchNew() {
-	redwrap.r(reddits).new(function (err, data, res) {
-		if (err || data.error) {
-			console.error('Error "' + (err || data.error) + '" when refreshing post list, retrying on next interval');
-			return;
-		}
-
-		for (var i = 0; i < data.data.children.length; ++i) {
-			var post = data.data.children[i].data;
-			if (lastSeen.indexOf(post.id) !== -1) {
-				console.log('found last post - stopping');
-				break; // already processed these posts
-			}
-			lastSeen.push(post.id);
-
-			console.log('annoucing new link: ' + post.title);
-			client.say(channelName, '[' + post.subreddit + '] [' + post.author + '] ' + ent.decode(post.title) + ' [ http://redd.it/' + post.id + ' ]' + (!post.is_self ? ' [ ' + post.url + ' ]' : '') + (post.over_18 ? ' [NSFW]' : ''));
-		}
-	});
-
-}
-
-
-if (!lastSeen.length) {
-	redwrap.r(reddits).new(function (err, data, res) {
-		if (err || data.error) {
-			console.error('Couldn\'t retrieve last post! Error: ' + (err || data.error));
-			process.exit(1);
-		}
-
-		for (var i = 0; i < data.data.children.length; ++i) {
-			var post = data.data.children[i].data;
-			lastSeen.push(post.id);
-		}
-
-		console.log('saved last posts');
 	});
 }
 
---
+++ plugins/reddit.js
@@ -0,0 +1,50 @@
+﻿var redwrap = require('redwrap');
+var ent = require('ent');
+
+var reddits = 'awwnime+pantsu+melanime+luckyyuri+kyoaniyuri+patchuu+moescape+imouto+ZettaiRyouiki';
+
+module.exports = function (client, channelName) {
+
+	var lastSeen = [];
+
+	function searchNew() {
+		redwrap.r(reddits).new(function (err, data, res) {
+			if (err || data.error) {
+				console.error('Error "' + (err || data.error) + '" when refreshing post list, retrying on next interval');
+				return;
+			}
+
+			for (var i = 0; i < data.data.children.length; ++i) {
+				var post = data.data.children[i].data;
+				if (lastSeen.indexOf(post.id) !== -1) {
+					console.log('found last post - stopping');
+					break; // already processed these posts
+				}
+				lastSeen.push(post.id);
+
+				console.log('annoucing new link: ' + post.title);
+				client.say(channelName, '[' + post.subreddit + '] [' + post.author + '] ' + ent.decode(post.title) + ' [ http://redd.it/' + post.id + ' ]' + (!post.is_self ? ' [ ' + post.url + ' ]' : '') + (post.over_18 ? ' [NSFW]' : ''));
+			}
+		});
+	}
+
+	if (!lastSeen.length) {
+		redwrap.r(reddits).new(function (err, data, res) {
+			if (err || data.error) {
+				console.error('Couldn\'t retrieve last post! Error: ' + (err || data.error));
+				process.exit(1);
+			}
+
+			for (var i = 0; i < data.data.children.length; ++i) {
+				var post = data.data.children[i].data;
+				lastSeen.push(post.id);
+			}
+
+			console.log('saved last posts');
+		});
+	}
+
+	setInterval(searchNew, 30 * 1000);
+
+	return {};
+}
