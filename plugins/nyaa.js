e83d7599b813150f601ed1c5fd2a4648f5a2b034
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-10 23:24:33 +0200

    Add nyaa plugin

---
+++ plugins/nyaa.js
@@ -0,0 +1,26 @@
+﻿var filesize = require('filesize');
+var nyaa = require("nyaatorrents");
+
+module.exports = function (client, channelName) {
+
+	var n = new nyaa('http://www.nyaa.eu');
+
+	function post(id) {
+		n.get(id, function (err, data) {
+			if (err) return;
+
+			client.say(channelName, data.name + ' - ' + filesize(data.size) + ' | S: ' + data.seeders + ' | L: ' + data.leechers + ' | http://www.nyaa.eu/?page=download&tid=' + data.id);
+		});
+	}
+
+	return {
+		messageHandler: function (from, message) {
+			var re = /http:\/\/www\.nyaa\.eu\/\?page=view&tid=(\d+)/gi;
+			var match;
+
+			while (match = re.exec(message)) {
+				post(match[1]);
+			}
+		}
+	};
+}
