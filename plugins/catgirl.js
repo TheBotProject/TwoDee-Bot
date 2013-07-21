3644fc5c4f36ed4154644eb6bea068f20ba5d6aa
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-21 18:08:25 +0200

    Add !catgirl command

---
+++ plugins/catgirl.js
@@ -0,0 +1,39 @@
+﻿var xml2js = require('xml2js');
+var request = require('request');
+
+module.exports = function (client, channelName) {
+
+	function random(min, max) {
+		return min + Math.floor(Math.random() * ((max - min) + 1));
+	}
+
+	function requestAndParse(url, cb) {
+		request(url, function (err, res, body) {
+			if (err) {
+				cb(err);
+				return;
+			}
+
+			var parser = new xml2js.Parser();
+			parser.parseString(body, cb);
+		});
+	}
+
+	return {
+		commands: {
+			catgirl: function (from, message) {
+				requestAndParse('http://safebooru.org/index.php?page=dapi&s=post&q=index&tags=cat_ears&limit=0', function (err, res) {
+					if (err) {
+						console.error('Catgirl error: ' + err);
+						return;
+					}
+
+					var rand = random(0, res.posts.$.count - 1);
+					requestAndParse('http://safebooru.org/index.php?page=dapi&s=post&q=index&tags=cat_ears&limit=1&pid=' + rand, function (err, res) {
+						client.say(channelName, res.posts.post[0].$.file_url);
+					});
+				});
+			}
+		}
+	};
+}
