ef026fa5c11396967ea241e5ad626086152276a2
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-22 23:04:01 +0200

    Reimplement time-ago to provide better accuracy

--- plugins/seen.js
+++ plugins/seen.js
@@ -1,6 +1,23 @@
-﻿var timeAgo = require('time-ago').timeago;
+﻿var second = 1000,
+	minute = second * 60,
+	hour = minute * 60,
+	day = hour * 24;
 
 module.exports = function (client, channelName) {
+
+	function ago(date) {
+		var diff = Date.now() - new Date(date).getTime();
+
+		if (diff > 100 * hour) { // > 100 hours
+			return Math.round(diff / day) + ' day' + (Math.round(diff / day) > 1 ? 's' : '') + ' ago';
+		} else if (diff > hour) {
+			return Math.round(diff / hour) + ' hour' + (Math.round(diff / hour) > 1 ? 's' : '') + ' ago';
+		} else if (diff > minute) {
+			return Math.round(diff / minute) + ' minute' + (Math.round(diff / minute) > 1 ? 's' : '') + ' ago';
+		} else {
+			return Math.round(diff / second) + ' second' + (Math.round(diff / second) !== 1 ? 's' : '') + ' ago';
+		}
+	}
 
 	var users = {};
 	setTimeout(function () {
@@ -38,12 +55,12 @@ module.exports = function (client, channelName) {
 						client.say(channelName, message + ' is here! I have no idea when he/she came though :(');
 					} else if (user.joined !== null) {
 						if (user.left !== null && user.joined < user.left) {
-							client.say(channelName, message + ' last joined ' + timeAgo(user.joined) + ', but left ' + timeAgo(user.left));
+							client.say(channelName, message + ' last joined ' + ago(user.joined) + ', but left ' + ago(user.left));
 						} else {
-							client.say(channelName, message + ' is here! He/She came ' + timeAgo(user.joined) + '!');
+							client.say(channelName, message + ' is here! He/She came ' + ago(user.joined) + '!');
 						}
 					} else if (user.joined === null && user.left !== null) {
-						client.say(channelName, 'I have no idea when ' + message + ' last joined, he/she left ' + timeAgo(user.left) + ' though!');
+						client.say(channelName, 'I have no idea when ' + message + ' last joined, he/she left ' + ago(user.left) + ' though!');
 					} else {
 						client.say(channelName, 'Something\'s wrong with me. Fix meeeh, fix meeeee :((((');
 						process.exit(1);
