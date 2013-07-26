c57ecaf57c42de28d5e35e7376d8d8dfc3349e1b
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-27 01:38:38 +0200

    Use created time instead of post ids

--- plugins/reddit.js
+++ plugins/reddit.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 module.exports = function (client, channelName) {
 
 	var reddits = {};
-	var lastSeen = [];
+	var lastUpdate = null;
 
 	function searchNew() {
 		redwrap.r(Object.keys(reddits).join('+')).new(function (err, data, res) {
@@ -14,34 +14,33 @@ module.exports = function (client, channelName) {
 				return;
 			}
 
-			for (var i = 0; i < data.data.children.length; ++i) {
-				var post = data.data.children[i].data;
-				if (lastSeen.indexOf(post.id) !== -1) {
-					console.log('found last post - stopping');
-					break; // already processed these posts
+			if (data.data.children.length) {
+				for (var i = 0; i < data.data.children.length; ++i) {
+					var post = data.data.children[i].data;
+					if (post.created_utc <= lastUpdate) break;
+
+					console.log('annoucing new link: ' + post.title);
+
+					var srData = reddits[post.subreddit.toLowerCase()];
+					var color = srData.color ? srData.color : '01,00';
+					client.say(channelName, '[\x03' + color + post.subreddit + '\x03] [' + post.author + '] ' + ent.decode(post.title) + ' [ http://redd.it/' + post.id + ' ]' + (!post.is_self ? ' [ ' + post.url + ' ]' : '') + (post.over_18 || srData.nsfl ? ' [NSFW]' : ''));
 				}
-				lastSeen.push(post.id);
 
-				console.log('annoucing new link: ' + post.title);
-
-				var srData = reddits[post.subreddit.toLowerCase()];
-				var color = srData.color ? srData.color : '01,00';
-				client.say(channelName, '[\x03' + color + post.subreddit + '\x03] [' + post.author + '] ' + ent.decode(post.title) + ' [ http://redd.it/' + post.id + ' ]' + (!post.is_self ? ' [ ' + post.url + ' ]' : '') + (post.over_18 || srData.nsfl ? ' [NSFW]' : ''));
+				lastUpdate = data.data.children[0].data.created_utc;
 			}
 		});
 	}
 
 	function init() {
-		if (!lastSeen.length) {
+		if (!lastUpdate) {
 			redwrap.r(Object.keys(reddits).join('+')).new(function (err, data, res) {
 				if (err || data.error) {
 					console.error('Couldn\'t retrieve last post! Error: ' + (err || data.error));
 					process.exit(1);
 				}
 
-				for (var i = 0; i < data.data.children.length; ++i) {
-					var post = data.data.children[i].data;
-					lastSeen.push(post.id);
+				if (data.data.children.length) {
+					lastUpdate = data.data.children[0].data.created_utc;
 				}
 
 				console.log('saved last posts');
@@ -61,11 +60,5 @@ module.exports = function (client, channelName) {
 		init();
 	});
 
-
-	setInterval(function () {
-		console.log('Cleaning memory');
-		lastSeen.splice(0, lastSeen.length - 50);
-	}, 24 * 3600 * 1000);
-
 	return {};
-}
+};
