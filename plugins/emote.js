88cb69eca87e19835e0fddce4b4931dcd9cfba72
HigherFive <highfivelolz@gmail.com>
2013-09-22 22:38:59 +0300

    Merging pet.js and pat.js into the generalized emote.js

---
+++ plugins/emote.js
@@ -0,0 +1,122 @@
+var fs = require('fs');
+var utils = require('../utils');
+
+module.exports = function (client) {
+
+	var savedEmotes = JSON.parse(fs.readFileSync(__dirname + '/.emotes', { encoding: 'utf8' }));
+
+	var emotes = {
+		// the first column is probability weights
+		// the second column is the emote message
+		//  can contain '{0}' - the target of the emote
+		//  and '{1}' - the initiator
+		// the third column is the text for the count report command
+
+		pat: [
+			[20, 'pats {0}\'s head.', 'regular pat(s)'],
+			[10, 'gently pats {0}.', 'gentle pat(s)'],
+			[ 4, 'sensually pats {0}.', 'sensual pat(s)'],
+			[ 1, 'gropes {0}\'s firm buttocks.', 'grope(s)']
+		],
+		pet: [
+			[1, 'pets {0}.', 'pet(s)'],
+			[1, 'gives a catgirl to {0}.', 'catgirl(s)']
+		],
+	};
+
+	for (var p in emotes) {
+		savedEmotes[p] = savedEmotes[p] || {};
+	}
+
+	var format = utils.format;
+	var random = utils.random;
+
+	function isValidName(msg) {
+		// msg is a string and expected to be the name of the target of the pat
+		// TODO: output is true iff msg is a valid IRC name
+		// maybe also check if the target is online ATM
+
+		return true;
+	}
+
+	var totalWeight = {};
+	for (var p in emotes) {
+		totalWeight[p] = 0;
+		for (i = 0; i < emotes[p].length; i++) {
+			totalWeight[p] += emotes[p][i][0];
+		}
+	}
+
+	var commands = {};
+
+	for (var p in emotes) {
+		commands[p] = (function (p) {
+			return function (from, channel, message) {
+				if (channel === client.nick) return; // don't care about PMs
+
+				if (!message) return;
+
+				if (!isValidName(message)) {
+					client.say(channel, '"' + message + '" doesn\'t seem to be a valid name.');
+					return;
+				}
+
+				var rnd = random(totalWeight[p]);
+				var i;
+				for (i = 0; rnd >= emotes[p][i][0]; i++) {
+					rnd -= emotes[p][i][0];
+				}
+
+				var tar = message.toLowerCase();
+
+				if (!savedEmotes[p][tar])
+					savedEmotes[p][tar] = {};
+
+				if (savedEmotes[p][tar][i])
+					savedEmotes[p][tar][i]++;
+				else
+					savedEmotes[p][tar][i] = 1;
+
+				var emote = emotes[p][i];
+				client.action(channel, format(emote[1], message, from));
+			};
+		})(p),
+
+		commands[p + 's'] = (function (p) {
+			return function (from, channel, message) {
+				if (channel === client.nick) { // respond to PMs because why not
+					channel = from;
+				}
+
+				if (!message) {
+					message = from;
+				}
+
+				if (!isValidName(message)) {
+					client.say(channel, '"' + message + '" doesn\'t seem to be a valid name.');
+					return;
+				}
+
+				var tar = message.toLowerCase();
+				if (!savedEmotes[p][tar]) {
+					client.say(channel, message + ' hasn\'t received any ' + p + 's yet ;___;');
+					return;
+				}
+
+				var rec = [];
+				for (var k in savedEmotes[p][tar]) {
+					rec.push(savedEmotes[p][tar][k] + ' ' + emotes[p][k][2]);
+				}
+
+				client.say(channel, message + ' has received ' + rec.join(', ') + '.');
+			}
+		})(p)
+	}
+
+	return {
+		commands: commands,
+		save: function () {
+			fs.writeFileSync(__dirname + '/.emotes', JSON.stringify(savedEmotes));
+		}
+	};
+};
