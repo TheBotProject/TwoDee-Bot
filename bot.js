TheBotProject/TwoDee-Bot

commit 253154d67ab368a4d9539e9860282e8e80972f87

Making some minor tweaks to the handling of joining and parting channels

bot.js
@@ -46,7 +46,9 @@ function activatePlugin(plugin) {
 
 	if (plugins[plugin].join) {
 		for (var chan in client.chans) { // if we're already connected but the plugin got reloaded, refresh it
-			plugins[plugin].join(chan);
+			if (state[chan].plugins.indexOf(plugin) !== -1) {
+				plugins[plugin].join(chan);
+			}
 		}
 	}
 }
@@ -75,7 +77,7 @@ client.on('part', function (channel, nick) {
 	if (nick === client.nick) {
 		state[channel].active = false;
 
-		for (var i in plugins) {
+		for (var i in state[channel].plugins) {
 			if (plugins[i].part) {
 				plugins[i].part(channel);
 			}
@@ -98,7 +100,6 @@ client.on('invite', function (channel, inviteUser) {
 			state[channel] = {};
 		}
 
-		state[channel].active = true;
 		state[channel].plugins = [];
 		for (var i = 0; i < config.plugins.length; ++i) {
 			state[channel].plugins.push(config.plugins[i]);
@@ -116,6 +117,8 @@ client.on('kick', function (channel, user) {
 client.on('join', function (channel, user) {
 	if (user === client.nick && state[channel]) {
 		console.log('Connected to ' + channel);
+
+		state[channel].active = true;
 
 		client.once('names' + channel, function (nicks) {
 			if (Object.keys(nicks).length === 1) {

commit 765217fbd9fbfdf456c672c9529f6cba9095c431

Fix bug introduced by earlier commit
253154d67ab368a4d9539e9860282e8e80972f87 accessed the activated plugins in a wrong way.

bot.js
@@ -77,9 +77,9 @@ client.on('part', function (channel, nick) {
 	if (nick === client.nick) {
 		state[channel].active = false;
 
-		for (var i in state[channel].plugins) {
-			if (plugins[i].part) {
-				plugins[i].part(channel);
+		for (var i=0; i < state[channel].plugins.length; ++i) {
+			if (plugins[state[channel].plugins[i]].part) {
+				plugins[state[channel].plugins[i]].part(channel);
 			}
 		}
 	} else if (state[channel].active && Object.keys(client.chans[channel].users).length === 2) {