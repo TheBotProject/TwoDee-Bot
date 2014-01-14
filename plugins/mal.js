64ea3e9737f45c62c7c01b221ecc32ab31f96282
HigherFive <highfivelolz@gmail.com>
2014-01-14 15:27:13 +0200

    Removing the !mal3 command
    It causes a crash when the requests made to mal-api.com fail with an error. Fixing it is not worth it considering it's virtually never used.

--- plugins/mal.js
+++ plugins/mal.js
@@ -2,12 +2,11 @@
 var request = require('request');
 var url = require('url');
 var ent = require('ent');
-var Q = require('q');
 
 module.exports = function (client) {
 
-	function searchAnime(query, limit, cb) {
-		google.resultsPerPage = limit;
+	function searchAnime(query, cb) {
+		google.resultsPerPage = 1;
 
 		google('site:myanimelist.net/anime/ ' + query, function (err, next, links) {
 			if (err) return;
@@ -17,71 +16,35 @@ module.exports = function (client) {
 				return;
 			}
 
-			if (limit === 1) {
-				var link = links[0];
-				var match = link.link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
-				if (match) {
-					var options = { uri: url.parse('http://mal-api.com/anime/' + match[1]) };
+			var link = links[0];
+			var match = link.link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
+			if (match) {
+				var options = { uri: url.parse('http://mal-api.com/anime/' + match[1]) };
 
-					options.timeout = 1000;
+				options.timeout = 1000;
 
-					request(options, function (err, r, data) {
-						if (err) {
-							cb(match[1]);
-							return;
-						}
-
-						if (r.statusCode < 200 || r.statusCode >= 300) {
-							cb(match[1]);
-							return;
-						}
-
-						data = JSON.parse(data);
-						if (data.error) {
-							cb(match[1]);
-						} else {
-							cb(data);
-						}
-					});
-				} else if (next) {
-					next();
-				} else {
-					cb(null);
-				}
-			} else {
-				Q.all(
-					links.map(function (link) {
-						var deferred = Q.defer();
-						var match = link.link.match(/^https?:\/\/myanimelist\.net\/anime\/(\d+)/);
-						if (match) {
-							request('http://mal-api.com/anime/' + match[1], function (err, r, data) {
-								if (err) {
-									deferred.reject(err);
-								} else {
-									var data = JSON.parse(data);
-									data.id = match[1];
-
-									deferred.resolve(data);
-								}
-							});
-						} else {
-							deferred.resolve(undefined);
-						}
-						return deferred.promise;
-					})
-				).then(function (datas) {
-					for (var i = 0; i < datas.length; ++i) {
-						var data = datas[i];
-						if (!data) continue;
-
-						if (data.error) {
-							cb(data.id);
-						} else {
-							cb(data);
-						}
+				request(options, function (err, r, data) {
+					if (err) {
+						cb(match[1]);
+						return;
 					}
 
-				}).done();
+					if (r.statusCode < 200 || r.statusCode >= 300) {
+						cb(match[1]);
+						return;
+					}
+
+					data = JSON.parse(data);
+					if (data.error) {
+						cb(match[1]);
+					} else {
+						cb(data);
+					}
+				});
+			} else if (next) {
+				next();
+			} else {
+				cb(null);
 			}
 		});
 	}
@@ -89,18 +52,7 @@ module.exports = function (client) {
 	return {
 		commands: {
 			mal: function (from, channel, message) {
-				searchAnime(message, 1, function (data) {
-					if (data === null) {
-						client.say(channel, 'Sorry, no results for: ' + message);
-					} else if (typeof data === 'object') {
-						client.say(channel, ent.decode(data.title) + ' (' + (data.episodes ? data.episodes : '?') + ' episodes) - http://myanimelist.net/anime/' + data.id);
-					} else {
-						client.say(channel, 'Couldn\'t parse anime info, here\'s the link though: http://myanimelist.net/anime/' + data);
-					}
-				});
-			},
-			mal3: function (from, channel, message) {
-				searchAnime(message, 3, function (data) {
+				searchAnime(message, function (data) {
 					if (data === null) {
 						client.say(channel, 'Sorry, no results for: ' + message);
 					} else if (typeof data === 'object') {
