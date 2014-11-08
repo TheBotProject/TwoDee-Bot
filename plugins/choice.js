5f09eb4b6d1e402ed3984b71f06cd00f0629598d
Alex <akolpa1990@gmail.com>
2014-11-09 00:02:51 +0100

    Added regex parsing for choice
    The regex parsing should add some stability to the choice parser. Also the plugin doesn't try to fix broken strings anymore. String parsing is hard enough as is.

--- plugins/choice.js
+++ plugins/choice.js
@@ -1,4 +1,3 @@
-﻿var readLine = require("csv");
 var utils = require('../utils');
 
 module.exports = function (client) {
@@ -15,16 +14,23 @@ module.exports = function (client) {
 				}
 
 				// if we have an odd number of "
-				// just assume the user missed the final one
+				// we tell the user something went wrong
 				var match;
 				if ((match = message.match(/"/g)) && (match.length % 2 === 1)) {
-					message += '"';
+					client.say(channel, from + ', you seem to be missing a " in your choices.');
+					return;
 				}
-
-				csv().from.string(message, { delimiter: ' ' }).to.array(function (data) {
-					data = data[0];
-					client.say(channel, from + ': ' + data[random(data.length)]);
-				});
+				
+				// This regex matches for words between spaces or all words between quotations
+				// Original solution: http://stackoverflow.com/a/366532
+				var choices = message.match(/[^\s"']+|"([^"]*)"|'([^']*)'/g);
+				
+				if (choices.length === 0) {
+					client.say(channel, 'Sorry ' + from + ', something went wrong while trying to parse your choices.');
+					return;
+				}
+				
+				client.say(channel, from + ': ' + choices[random(choices.length)]);
 			}
 		}
 	};
