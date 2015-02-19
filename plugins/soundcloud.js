a75b2413de5320fe825158dd105ec5a2dc64a7a5
Alexander Varbanov <highfivelolz@gmail.com>
2015-02-19 10:51:42 +0200

    Refactoring the soundcloud plugin
    allowing it to report playlists as well and modifying the format of the
    report messages

--- plugins/soundcloud.js
+++ plugins/soundcloud.js
@@ -12,11 +12,43 @@ try {
 	return;
 }
 
-function querySoundcloud(link, cb) {
-	var url = 'https://api.soundcloud.com/resolve.json?'
-		+ 'client_id=' + clientId + '&'
-		+ 'url=' + link;
+// song URLs look like this: https://soundcloud.com/<artist name>/<song name>
+var songRegex = /^(?:https?:\/\/)?soundcloud\.com\/([^/ ]+)\/([^/ ]+)\/?$/gi;
+// set URLs look like this: https://soundcloud.com/<artist name>/sets/<set name>
+var setRegex = /^(?:https?:\/\/)?soundcloud\.com\/([^/ ]+)\/sets\/([^/ ]+)\/?$/gi;
 
+function parseLinks(str) {
+	var strArr = str.trim().split(/\s+/);
+	var match;
+	var matches = [];
+	for (var i = 0; i < strArr.length; i++) {
+		// song URLs may have a query string like this:
+		// https://soundcloud.com/<artist name>/<song name>?in=<artist name>/<set name>
+		// we don't care about it, so we just drop it
+		strArr[i] = strArr[i].split('?')[0];
+		
+		if (match = songRegex.exec(strArr[i])) {
+			matches.push({
+				type: 'track',
+				url: match[0],
+				artist: match[1],
+				title: match[2]
+			});
+		} else if (match = setRegex.exec(strArr[i])) {
+			matches.push({
+				type: 'playlist',
+				url: match[0],
+				artist: match[1],
+				title: match[2]
+			});
+		}
+	}
+
+	return matches;
+}
+
+
+function querySoundcloud(url, cb) {
 	request.get(url, function(err, resp, body) {
 		if (err) {
 			cb(new Error('Something went wrong while querying Soundcloud: ' + err.message), null);
@@ -48,8 +80,9 @@ function format(data) {
 	var title = data.title;
 	var artist = data.user.username;
 	var link = data.permalink_url;
+	var count = data.track_count;
 
-	return '[' + artist + '] ' + title + (genre ? ' [' + genre + ']' : '') + ' [' + durationFormat(Math.ceil(duration / 1000)) + '] [ ' + link + ' ]';
+	return '[' + artist + '] ' + title + (genre ? ' \x0302#' + genre + '\x03' : '') + (count ? ' (' + count + ' tracks)' : '') + ' [' + durationFormat(Math.ceil(duration / 1000)) + '] [ ' + link + ' ]';
 }
 
 module.exports = function (client) {
@@ -58,31 +91,31 @@ module.exports = function (client) {
 			if (to === client.nick) {
 				return;
 			}
-
-			var match, re = /(:?^|\s)(?:https?:\/\/)?(?!=api\.)soundcloud\.com\/\S*/gi;
-			while (match = re.exec(msg)) {
-				(function (link) {
-					querySoundcloud(link, function (err, result) {
+			
+			var matches = parseLinks(msg);
+			for (var i = 0; i < matches.length; i++) {				
+				(function (match) {
+					var requestURL = 'https://api.soundcloud.com/resolve.json?'
+							+ 'client_id=' + clientId + '&'
+							+ 'url=' + encodeURIComponent(match.url);
+					
+					querySoundcloud(requestURL, function (err, result) {
 						if (err) {
-							console.error('Error while processing ' + link);
+							console.error('Error while processing ' + requestURL);
 							console.error(err);
 
 							return;
 						} else if (result.errors) {
-							console.error('Error while processing ' + link);
-							console.error(JSON.stringify(results.errors));
-
+							console.error('Error while processing ' + requestURL);
+							console.error(JSON.stringify(result.errors));
+							
 							return;
-						} else if (result.kind !== 'track') {
-							console.error(link + ' is not a track; it\'s a ' + result.kind);
-
-							return;
+						} else  {
+							var msg = format(result);
+							client.say(to, msg);
 						}
-
-						var msg = format(result);
-						client.say(to, msg);
 					});
-				})(match[0].trim());
+				})(matches[i]);
 			}
 		}
 	};
