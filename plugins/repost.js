0bc5bee790ece6f3dc7560ad62ce532e9a64ef0e
Alexander Varbanov <highfivelolz@gmail.com>
2015-02-23 14:16:29 +0200

    Rearranging if statement to reduce pyramid height

--- plugins/repost.js
+++ plugins/repost.js
@@ -48,37 +48,39 @@ module.exports = function (client) {
 					}
 					try {
 						var parsed = JSON.parse(res.body).results;
-						if (parsed.length !== 0) {
-							parsed = parsed.sort(function (a, b) { return a.age - b.age; });
-							parsed = parsed.filter(function (v) {
-								var distance = parseFloat(v.distance);
-								return Math.round(distance * 1000) <= 8;
-							});
+						if (parsed.length === 0) {
+							client.say(to, 'No reposts found for ' + msg);
 							
-							if (parsed.length > 1) {
-								var responseMsg = parsed.length + ' posts found: ';
-								
-								for (var i = 0; i < parsed.length; i++) {
-									responseMsg += (parsed[i].nsfw ? '[\x0304NSFW\x03] ' : '') +
-									'[' + parsed[i].sourceName + '] ' +
-									'[ http://reddit.com/' + parsed[i].externalId + ' ] ' +
-									'(' + humanize(parsed[i].age) + ') ' +
-									(i < parsed.length - 1 ? '| ' : '');
-								}
-								client.say(to, responseMsg);
-							} else if (parsed.length === 1) {
-								var found = parsed[0];
-								
-								client.say(to,
-									'Post found: ' +
-									(found.nsfw ? '[\x0304NSFW\x03] ' : '') +
-									'[' + found.sourceName + '] ' +
-									'[' + found.userName + '] ' +
-									found.title + ' [ http://reddit.com/' + found.externalId + ' ] ' +
-									'(' + humanize(found.age) + ') ');
-							} else {
-								client.say(to, 'No reposts found for ' + msg);
+							return;
+						}
+						
+						parsed = parsed.sort(function (a, b) { return a.age - b.age; });
+						parsed = parsed.filter(function (v) {
+							var distance = parseFloat(v.distance);
+							return Math.round(distance * 1000) <= 8;
+						});
+						
+						if (parsed.length > 1) {
+							var responseMsg = parsed.length + ' posts found: ';
+							
+							for (var i = 0; i < parsed.length; i++) {
+								responseMsg += (parsed[i].nsfw ? '[\x0304NSFW\x03] ' : '') +
+								'[' + parsed[i].sourceName + '] ' +
+								'[ http://reddit.com/' + parsed[i].externalId + ' ] ' +
+								'(' + humanize(parsed[i].age) + ') ' +
+								(i < parsed.length - 1 ? '| ' : '');
 							}
+							client.say(to, responseMsg);
+						} else if (parsed.length === 1) {
+							var found = parsed[0];
+							
+							client.say(to,
+								'Post found: ' +
+								(found.nsfw ? '[\x0304NSFW\x03] ' : '') +
+								'[' + found.sourceName + '] ' +
+								'[' + found.userName + '] ' +
+								found.title + ' [ http://reddit.com/' + found.externalId + ' ] ' +
+								'(' + humanize(found.age) + ') ');
 						} else {
 							client.say(to, 'No reposts found for ' + msg);
 						}
