e7deccdbf93018e4ddf360f1161bd1f8801ad6b1
Sebastian Mayr <git@smayr.name>
2018-04-28 14:01:53 -0400

    Drop azure server from bot

--- bot.js
+++ bot.js
@@ -2,17 +2,12 @@ var irc = require('irc');
 var fs = require('fs');
 var module = require('module');
 
-var server = require(__dirname + '/server');
 /*
 var channelName = '#TwoDeeTest';
 var reddits = 'all';
 */
-var config = JSON.parse(
-  fs.readFileSync(__dirname + '/config.json', { encoding: 'utf8' })
-);
-var state = JSON.parse(
-  fs.readFileSync(__dirname + '/state.json', { encoding: 'utf8' })
-);
+var config = require('./config.json');
+var state = require('./state.json');
 
 var plugins = {};
 
@@ -318,7 +313,6 @@ config.plugins.forEach(function(plugin) {
   });
 })();
 
-server(client);
 process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
 
 client.on('registered', function() {
